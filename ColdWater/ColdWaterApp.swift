3dd78d3afb2bea3714c69d6f2254dc2f29a5d607
catherinecshi <123045203+catherinecshi@users.noreply.github.com>
2025-07-10 15:28:41 -0400

    Initial Commit

---
+++ ColdWater/ColdWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColdWaterApp.swift
+//  ColdWater
+//
+//  Created by Shi Catherine on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ColdWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
